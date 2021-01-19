a4f1ea737aef4107079ea2cbf9716b28f589f234
Xx-Ashutosh-xX <ashutosh.saxena.2001@gmail.com>
2021-01-19 16:49:16 +0530

    Modified settings.py

--- Web_App/settings.py
+++ Web_App/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'io6sce#5o(g%gw=$kfwaec34!s*yd^qge28*+df3&_!2wa3=7q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['supreme-physical-gamer.herokuapp.com']
+ALLOWED_HOSTS = ['supreme-physical-gamer.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
